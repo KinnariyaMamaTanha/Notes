f544423c5de14ebbbe39be403940009e73390003
KinnariyaMamaTanha <2823324288@qq.com>
2024-01-02 18:17:21 +0800

    1.2.2024

--- Coding Languages/Python/Data structure/data_structure&algorithm/base_convert.py
+++ Coding Languages/Python/Data structure/data_structure&algorithm/base_convert.py
@@ -1,6 +1,9 @@
+"""Base convertion function."""
 from nstack import nstack
+
+
 def base_convert(number: str | int, new_base: int) -> str:
-    """This is a function which convert a decimal number to numbers with any other bases under 10"""
+    """Convert a decimal number to numbers.with any other bases under 10."""
     tmp = nstack()
     number = int(number)
     while number != 0:
