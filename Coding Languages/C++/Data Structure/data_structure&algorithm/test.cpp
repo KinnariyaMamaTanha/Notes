832b02b65a301e346a0429b74cc382221284f34f
KinnariyaMamaTanha <2823324288@qq.com>
2024-01-08 21:27:46 +0800

    1.8.2024

--- Coding Languages/C++/Data Structure/data_structure&algorithm/test.cpp
+++ Coding Languages/C++/Data Structure/data_structure&algorithm/test.cpp
@@ -9,11 +9,6 @@
 using namespace std;
 int main()
 {
-	int a[] = { 2, 12, 10, 13, 5, 20, 14, 17, 7, 3, 15, 30 };
-	cout << order_statistic(a, 10, 4) << endl;
-	quick_sort(a, 10);
-	for (int i = 0; i < 10; ++i)
-		cout << a[i] << ' ';
 	return 0;
 }
 // 2 -10 20 0 -9 3 14 0 30 6
